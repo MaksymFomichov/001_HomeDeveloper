ce9a5c45d374b8939d9dd2220c60d90d87ac5352
MaksymFomichov <m.fomichov@gmail.com>
2017-11-02 17:37:00 +0200

    update

--- src/main/resources/6.sql
+++ src/main/resources/6.sql
@@ -1,11 +1,11 @@
 /*Вычислить, среднюю ЗП программистов в проекте, который приносит наименьшую прибыль.*/
 
-SELECT sum(salary) / count(salary)
+SELECT sum(developers.salary) / count(developers.salary)
 FROM developers, developers_projects, projects
 WHERE developers_projects.developers_id = developers.id AND
       (developers_projects.projects_id = projects.id AND
        (projects.id, projects.cost) IN (
          SELECT
            projects.id,
-           min(cost)
+           min(projects.cost)
          FROM projects))
