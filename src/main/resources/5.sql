4d4925df3d5a36112976b1e998a91d86900bcb9a
MaksymFomichov <m.fomichov@gmail.com>
2017-11-27 23:12:28 +0200

    add 5 example

--- src/main/resources/5.sql
+++ src/main/resources/5.sql
@@ -2,13 +2,30 @@
  для каждой из компаний .*/
 
 SELECT
-compani,
-customer,
-project,
-min(cost)
-FROM customers, projects, customers_projects, companies, companies_projects
-WHERE customers_projects.projects_id = projects.id AND
-customers_projects.customers_id = customers.id AND
-companies_projects.projects_id = projects.id AND
-companies_projects.companies_id = companies.id
-GROUP BY compani
+  compani,
+  customer,
+  project,
+  min(cost)
+FROM (
+       SELECT
+         customers_projects.customers_id AS id_customer,
+         companies_projects.projects_id  AS id_project,
+         companies_projects.companies_id AS id_company
+       FROM customers_projects
+         LEFT JOIN companies_projects ON customers_projects.projects_id = companies_projects.projects_id
+
+       UNION
+
+       SELECT
+         customers_projects.customers_id,
+         companies_projects.projects_id,
+         companies_projects.companies_id
+       FROM customers_projects
+         RIGHT JOIN companies_projects ON customers_projects.projects_id = companies_projects.projects_id
+     ) AS tableMain
+
+  LEFT JOIN projects ON tableMain.id_project = projects.id
+  LEFT JOIN companies ON tableMain.id_company = companies.id
+  LEFT JOIN customers ON tableMain.id_customer = customers.id
+GROUP BY id_customer, id_company
+ORDER BY id_company;
